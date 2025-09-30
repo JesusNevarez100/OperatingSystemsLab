3ba61ac95e5c4e6c5f0f01d5bd16585595993cb3
jamie <james.farrow@student.nmt.edu>
2025-09-30 14:59:07 -0600

    Completed code for exercise 3, just had to add the dummy file, did nice on push with exercise 1

--- lab3_fn/xv6-tmp/user/lab3test.c
+++ lab3_fn/xv6-tmp/user/lab3test.c
@@ -1,13 +1,36 @@
 #include "kernel/types.h"
+#include "kernel/stat.h"
+#include "kernel/fcntl.h"
 #include "user/user.h"
 
-int main(int argc, char *argv[]) {
-    if(argc != 3){
-        printf("Usage: nice pid priority\n");
-        exit(1);
-    }
-    int pid = atoi(argv[1]);
-    int prio = atoi(argv[2]);
-    set_priority(pid, prio); // syscall from lab 2
-    exit(0);
+int main(int argc, char *argv[])
+{
+	int k, n, id;
+	volatile long x = 0, z;
+	
+	if(argc < 2)
+		n = 1;		// default value
+	else
+		n = atoi(argv[1]);  // from command line
+	
+	if(n < 0 || n > 20)
+		n = 2;
+	
+	id = 0;
+	for(k = 0; k < n; k++){
+		id = fork();
+		if(id < 0){
+			printf("%d failed in fork!\n", getpid());
+		}
+		else if(id > 0){ // parent
+			printf("Parent %d creating child %d\n", getpid(), id);
+		} else{ // child
+			printf("Child %d created\n", getpid());
+			for(z = 0; z < 8000000000; z += 1)
+				x = x + 1;
+			printf("Child %d terminated\n", getpid());
+			break;
+		}
+	}
+	exit(0);
 }
