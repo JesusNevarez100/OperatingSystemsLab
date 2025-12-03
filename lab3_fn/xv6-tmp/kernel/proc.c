010db2ca65145407989e619c79f3e63f059b5f0b
JesusNevarez100 <jesus.nevarez@student.nmt.edu>
2025-12-03 01:07:52 +0000

    lab3 commit

--- lab3_fn/xv6-tmp/kernel/proc.c
+++ lab3_fn/xv6-tmp/kernel/proc.c
@@ -424,38 +424,38 @@ void
 scheduler(void)
 {
   struct proc *p;
+  struct proc *best; // <--- Holds best nice value's
+
   struct cpu *c = mycpu();
 
   c->proc = 0;
   for(;;){
-    // The most recent process to run may have had interrupts
-    // turned off; enable them to avoid a deadlock if all
-    // processes are waiting. Then turn them back off
-    // to avoid a possible race between an interrupt
-    // and wfi.
     intr_on();
     intr_off();
 
-    int found = 0;
+    best = 0;
+
+    // find proc with lowest nice value, must be runnable
     for(p = proc; p < &proc[NPROC]; p++) {
       acquire(&p->lock);
       if(p->state == RUNNABLE) {
-        // Switch to chosen process.  It is the process's job
-        // to release its lock and then reacquire it
-        // before jumping back to us.
-        p->state = RUNNING;
-        c->proc = p;
-        swtch(&c->context, &p->context);
-
-        // Process is done running for now.
-        // It should have changed its p->state before coming back.
-        c->proc = 0;
-        found = 1;
+        if (best == 0 || p->nice < best->nice){
+          if (best) release(&best->lock);
+          best = p;
+          continue;  
+        }
       }
       release(&p->lock);
     }
-    if(found == 0) {
-      // nothing to run; stop running on this core until an interrupt.
+    
+    if (best){
+      best->state = RUNNING;
+      c->proc = best;
+      swtch(&c->context, &best->context);
+
+      c->proc = 0;
+      release(&best->lock);
+    } else {
       asm volatile("wfi");
     }
   }
@@ -728,15 +728,19 @@ cps(void)
 	
 	// Loop over process table looking for process with pid
 
-	printf("name \t pid \t state \t priority \n");
+	printf("name\t\t pid\t state\t priority\n");
 	for(p = proc; p < &proc[NPROC]; p++){
-		if(p->state == SLEEPING)
-			printf("%s \t %d \t SLEEEPING \t %d \n", p->name, p->pid, p->nice);
-		else if(p->state == RUNNING)
-			printf("%s \t %d \t RUNNING \t %d \n", p->name, p->pid, p->nice);
-		else if(p->state == RUNNABLE)
-			printf("%s \t %d \t RUNNABLE \t %d \n", p->name, p->pid, p->nice);	
+    acquire(&p->lock);
+    if (p->state != UNUSED) {
+      printf("%s\t\t %d\t %s\t %d\n",
+            p->name, p->pid,
+            (p->state == RUNNABLE) ? "RUNNABLE" :
+            (p->state == RUNNING)  ? "RUNNING" :
+            (p->state == SLEEPING)   ? "SLEEPING" : "??",
+            p->nice);
+    }
+    release(&p->lock);
 	}
 	
-	return 22;
+	return 0;
 }
